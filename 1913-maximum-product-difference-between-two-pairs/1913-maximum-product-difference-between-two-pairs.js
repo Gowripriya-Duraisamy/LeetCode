4473219bed2c1788f3d45564912b884defbfed27
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-07-17 13:27:07 +0530

    Time: 62 ms (94.66%), Space: 44.8 MB (58.46%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -3,6 +3,15 @@
  * @return {number}
  */
 var maxProductDifference = function (nums) {
-    nums.sort((a, b) => a - b);
-    return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
+       const min1 = Math.min(...nums);
+    const max1 = Math.max(...nums);
+    const minIndex = nums.indexOf(min1);
+    [nums[minIndex], nums[nums.length - 1]] =  [ nums[nums.length - 1], nums[minIndex]] 
+    nums.pop();
+    const maxIndex = nums.indexOf(max1);
+    [nums[maxIndex], nums[nums.length - 1]] =  [ nums[nums.length - 1], nums[maxIndex]] 
+    nums.pop();
+    const min2 = Math.min(...nums);
+    const max2 = Math.max(...nums);
+    return (max2 * max1) - (min1* min2)
 };
