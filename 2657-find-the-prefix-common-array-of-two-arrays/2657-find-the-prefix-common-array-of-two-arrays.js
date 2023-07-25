e97b702daa2657cccc1f7c477d7ae5b7956d0ad6
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-07-25 13:11:31 +0530

    Time: 108 ms (78.86%), Space: 48.1 MB (49.59%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.js
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var findThePrefixCommonArray = function (A, B) {
-    const resultArr = [];
+  const resultArr = [];
   let alreadyPrefixedCount = 0;
   const map = new Map();
 
@@ -12,18 +12,8 @@ var findThePrefixCommonArray = function (A, B) {
     if (A[i] === B[i]) {
       alreadyPrefixedCount++;
     } else {
-      if (map.get(A[i])) {
-        alreadyPrefixedCount++;
-        map.delete(A[i]);
-      } else {
-        map.set(A[i], 1);
-      }
-      if (map.get(B[i])) {
-        alreadyPrefixedCount++;
-        map.delete(B[i]);
-      } else {
-        map.set(B[i], 1);
-      }
+        map.get(A[i]) ? alreadyPrefixedCount++ : map.set(A[i], 1)
+        map.get(B[i]) ? alreadyPrefixedCount++ : map.set(B[i], 1)
     }
     resultArr.push(alreadyPrefixedCount);
   }
