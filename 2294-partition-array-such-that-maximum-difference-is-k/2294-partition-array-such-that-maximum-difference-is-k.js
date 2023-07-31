05e10951fb177e9dcd4ce0bf89187c4c0f734593
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-07-31 12:20:58 +0530

    Time: 173 ms (100.00%), Space: 54 MB (92.50%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.js
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.js
@@ -8,24 +8,13 @@ var partitionArray = function(nums, k) {
     let subSequenceCount = 0;
     let min = nums[0];
     let i=1;
-    let indexedUpto = 0;
     for(; i< nums.length; i++) {
         const diff = nums[i] - min;
-        if(diff === k) {
-            indexedUpto = i;
-            i++;
-            min !== nums[i] && subSequenceCount++;
-            min = nums[i]
-        } else if(diff > k) {
-            indexedUpto = i-1;
+        if(diff > k) {
             subSequenceCount++;
             min = nums[i];
         }
     }
-    if(i === nums.length) {
-        if(nums[i-1] !== nums[i-2] ) ++subSequenceCount;
-        if(nums[i-1] === nums[i-2] && indexedUpto !== i-2) ++subSequenceCount;
-        if(!subSequenceCount) ++subSequenceCount;
-    }
-    return subSequenceCount;
+   
+    return ++subSequenceCount;
 };
