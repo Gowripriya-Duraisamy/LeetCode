3923bb0965bad98200c54c94a443261375e68ad9
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-08-19 15:51:13 +0530

    Time: 68 ms (53.14%), Space: 48.6 MB (57.14%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function(root) {
+     const postOrderTraversal = (bstRoot) => {
+        if(!bstRoot.left && !bstRoot.right) {
+          if(bstRoot.val === 0 || bstRoot.val === 1) return bstRoot.val;
+          return;
+      }
+      if(bstRoot.left && bstRoot.right) {
+        const leftValue = postOrderTraversal(bstRoot.left);
+        const rightValue = postOrderTraversal(bstRoot.right);
+        if(bstRoot.val === 2 ) {
+          if(leftValue || rightValue) return 1;
+          return 0;
+        } else if(bstRoot.val === 3) {
+            if(leftValue && rightValue) return 1;
+            return 0;
+        }
+        else {
+          return bstRoot.val;
+        }
+      }
+     }
+      const result = postOrderTraversal(root);
+      return result === 0 ? false : true;
+};
