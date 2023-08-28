0a9cc4f5e0f895af8b12e063df16f02392ae3700
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-08-28 13:36:49 +0530

    Time: 68 ms (73.05%), Space: 45.1 MB (80.86%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,28 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+    const resultArr = [];
+    const traverse = (node) => {
+        if(!node) return;
+        let totalChildrenLength = node.children.length;
+        resultArr.push(node.val);
+        let i=0;
+        while (i < totalChildrenLength) {
+            traverse(node.children[i]);
+            i++;
+        }
+        return;
+    }
+    traverse(root);
+    return resultArr;
+};
