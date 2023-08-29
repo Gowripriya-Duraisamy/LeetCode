1a439f84997fa16be41234a2450698a56094f0b3
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-08-29 13:41:28 +0530

    Time: 55 ms (88.56%), Space: 45.3 MB (55.68%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,16 +11,6 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    let max = 0;
-    const traverse = (node) => {
-      if(!node) return 0;
-      const left = traverse(node.left) + 1;
-      const right = traverse(node.right) + 1;
-      max = max > left ? max : left;
-      max = max > right ? max :  right;
-      return left > right ? left : right;
-    }
-
-    traverse(root)
-    return max;
+      if(!root) return 0;
+      return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
