d44a10c1532c5dfdf0c31e2a49cf059e1f91bc35
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-09-04 13:19:25 +0530

    Time: 53 ms (64.71%), Space: 42.2 MB (56.86%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.js
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.js
@@ -11,28 +11,15 @@
  * @return {TreeNode}
  */
 
-const sumTree = root => {
-    let sum = 0;
-    const traverse = (node) => {
-        if (!node) return;
-        sum += node.val;
-        traverse(node.left);
-        traverse(node.right);
-        return;
-    }
-    traverse(root);
-    return sum;
-}
 
 var bstToGst = function (root) {
-    let totalSum = sumTree(root);
+    let totalSum = 0;
     const traverse = node => {
         if (!node) return;
-        !!node.left && traverse(node.left);
-        const temp = node.val;
-        node.val = totalSum;
-        totalSum -= temp;
         !!node.right && traverse(node.right);
+        totalSum += node.val;
+        node.val = totalSum;
+        !!node.left && traverse(node.left);
         return;
     }
     traverse(root);
