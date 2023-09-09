57a1c6e1e55fb612468b1ae0bc792056ed579bb3
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2023-09-09 16:20:37 +0530

    Time: 1883 ms (20.00%), Space: 50.1 MB (100.00%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+
+var FindElements = function(root) {
+    this.root = root;
+    const traverse = (node, parentVal) => {
+        if(!node) return;
+        node.val = parentVal;
+        node.left && traverse(node.left, (2*parentVal) + 1);
+        node.right && traverse(node.right, (2*parentVal)+ 2)
+        return;
+    }
+    traverse(this.root, 0);
+    return this;
+};
+
+/** 
+ * @param {number} target
+ * @return {boolean}
+ */
+FindElements.prototype.find = function(target) {
+    let isExists = false;
+    const traverse = (node) => {
+        if(!node) return;
+        if(!!isExists) return;
+        if(node.val === target) {
+            isExists = true;
+            return;
+        }
+        traverse(node.left);
+        traverse(node.right);
+        return;
+    }
+    traverse(this.root);
+    return isExists;
+};
+
+/** 
+ * Your FindElements object will be instantiated and called as such:
+ * var obj = new FindElements(root)
+ * var param_1 = obj.find(target)
+ */
