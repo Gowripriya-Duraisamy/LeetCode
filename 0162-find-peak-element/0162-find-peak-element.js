5993b3d1bc635c9012a63dd0ba53a787e7bfb1cb
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2024-03-29 12:35:03 +0530

    Time: 58 ms (21.25%), Space: 48.4 MB (93.63%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,10 +3,15 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
-    if(nums.length === 1) return 0;
-    for (let i = 0; i < nums.length; i++) {
-        if(i-1 === -1 && nums[i+1] < nums[i] ) return i;
-        else if(i+1 >= nums.length && nums[i-1] < nums[i]) return i;
-        else if (nums[i] > nums[i - 1]  && nums[i] > nums[i + 1]) return i;
+    if (nums.length === 1) return 0;
+    let start = 0;
+    let end = nums.length - 1;
+    while (start <= end) {
+        if (start - 1 === -1 && nums[start + 1] < nums[start]) return start;
+        if (end + 1 === nums.length && nums[end - 1] < nums[end]) return end;
+        if (nums[start] > nums[start + 1] && nums[start] > nums[start - 1]) return start;
+        if (nums[end] > nums[end + 1] && nums[end] > nums[end - 1]) return end;
+        start++;
+        end--;
     }
 };
