cbbcecf0fee955d020eb6508b7b9f50fdf91d0c7
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2024-03-31 14:47:42 +0530

    Time: 266 ms (33.04%), Space: 78.7 MB (45.22%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} spells
+ * @param {number[]} potions
+ * @param {number} success
+ * @return {number[]}
+ */
+var successfulPairs = function (spells, potions, success) {
+    const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length - 1;
+    let resultIndex = -1;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (arr[mid] >= target) {
+        resultIndex = mid;
+        right = mid-1;
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      }
+    }
+    return resultIndex;
+  };
+
+  potions = potions.sort((a, b) => a - b);
+  for (let i = 0; i < spells.length; i++) {
+    const startVal = Math.ceil(success / spells[i]);
+    const index = binarySearch(potions, startVal);
+    spells[i] = index >= 0 ? potions.length - index : 0;
+  }
+  return spells;
+};
