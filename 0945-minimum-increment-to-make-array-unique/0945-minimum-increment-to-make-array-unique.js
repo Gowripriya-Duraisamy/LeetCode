2790c299392635a30dd3ef285b4f738bcaa5d590
Gowripriya-Duraisamy <129388677+Gowripriya-Duraisamy@users.noreply.github.com>
2024-06-15 12:03:36 +0530

    Time: 214 ms (15.25%), Space: 60.7 MB (89.83%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.js
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.js
@@ -3,15 +3,18 @@
  * @return {number}
  */
 var minIncrementForUnique = function (nums) {
-    nums.sort((a, b) => a - b); 
+    nums.sort((a, b) => a - b); // Sort the array in ascending order
 
-    let count = 0; 
+    let count = 0; // Initialize the counter for moves
+    let neededValue = nums[0]; // Start with the first value
 
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] <= nums[i - 1]) {
-            count += nums[i - 1] + 1 - nums[i];
-            nums[i] = nums[i - 1] + 1;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < neededValue) {
+            // If the current number is less than the needed value, increment it
+            count += neededValue - nums[i];
+            nums[i] = neededValue; // Update the current number to the needed value
         }
+        neededValue = nums[i] + 1; // Update needed value for the next element
     }
 
     return count;
